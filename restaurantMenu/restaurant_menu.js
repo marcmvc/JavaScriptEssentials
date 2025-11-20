44b183ba4d573ec3b6d56e837ef82dfc43d2dfbc
Marcelo Vallejo <marcmvc@hotmail.com>
2025-11-19 22:21:29 -0500

    Update Array Loop

--- restaurantMenu/restaurant_menu.js
+++ restaurantMenu/restaurant_menu.js
@@ -1,6 +1,7 @@
 const breakfastMenu = ['Pancakes', 'Eggs Benedict', 'Oatmeal', 'Frittata'];
 const mainCourseMenu = ['Steak', 'Pasta', 'Burger', 'Salmon'];
 const dessertMenu = ['Cake', 'Ice Cream', 'Pudding', 'Fruit Salad'];
+const breakfastMenuWithPrices = ['Pancakes - $6', 'Eggs Benedict - $12', 'Oatmeal - $16', 'Frittata - $20'];
 
 const breakfastMenuItemsHTML = breakfastMenu.map((item, index) => `<p>Item ${index + 1}: ${item}</p>`).join('');
 document.getElementById('breakfastMenuItems').innerHTML = breakfastMenu;
@@ -14,3 +15,7 @@ for (let i = 0; i < dessertMenu.length; i++) {
     dessertItem += `<p>Item ${i + 1}: ${dessertMenu[i]}</p>`;
 }
 document.getElementById('dessertMenuItems').innerHTML = dessertItem;
+
+
+let breakfastMenuItemsWithPricesHTML = breakfastMenuWithPrices.map((item, index) => `<p>Item ${index + 1}: item ${item} </p>`).join('')
+document.getElementById('breakfastMenuItemsWithPrices').innerHTML = breakfastMenuItemsWithPricesHTML;
